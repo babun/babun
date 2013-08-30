1636a9a25030b4d9a7045a3b5304c31280cc20a3
Tom Bujok <tom.bujok@gmail.com>
2013-08-30 14:55:53 +0100

    User agent externalised. setup.sh extended

--- src/setup.sh
+++ src/setup.sh
@@ -4,7 +4,27 @@
 # 
 echo "Setting up babun!"
 
-#configure bark
+#functions
+function contains {
+	return $(( grep -Fxq "$1" $2 ))
+}
+
+# babun config folder
+if [ ! -d /etc/babun ]; then
+	mkdir /etc/babun
+fi
+
+# bark config file
+if [ ! -f /etc/babun/bark.conf ]; then
+    touch /etc/babun/bark.conf
+fi
+
+# babun config file
+if [ ! -f /etc/babun/babun.conf ]; then
+    touch /etc/babun/babun.conf
+fi
+
+# make bark executable
 chmod +x /usr/local/bin/bark
 
 exit 0

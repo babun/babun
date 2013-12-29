ad23cb51feb52dc17d2c99ba26633818c0cb6957
Tom Bujok <tom.bujok@gmail.com>
2013-12-29 18:37:15 +0100

    Changing modularization

---
+++ babun-cygwin/cygwin.bat
@@ -0,0 +1,11 @@
+mkdir D:\AAA\32\cygwin
+
+setup-x86.exe ^
+--quiet-mode ^
+--local-install ^
+--local-package-dir D:\AAA\32\packages ^
+--root D:\AAA\32\cygwin ^
+--no-shortcuts ^
+--no-startmenu ^
+--no-desktop ^
+--packages cron,shutdown,openssh,ncurses,vim,nano,unzip,curl,rsync,ping,links,wget,httping,time

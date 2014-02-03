babfbedc3539b3b8f56910764001dbe48f9858f3
Tom Bujok <tom.bujok@gmail.com>
2014-02-04 00:24:19 +0100

    Installer script added

---
+++ babun-dist/install/install.bat
@@ -0,0 +1,51 @@
+@echo off
+setlocal enableextensions enabledelayedexpansion
+
+set SCRIPT_PATH=%~dpnx0
+set SCRIPT_PATH=%SCRIPT_PATH:\=/%
+
+set BABUN_HOME=%USERPROFILE%\.babun
+set CYGWIN_HOME=%BABUN_HOME%\cygwin
+
+set BABUN_ZIP=%SCRIPT_PATH%/babun.zip
+set UNZIPPER=%SCRIPT_PATH%/unzip.exe
+
+set SETPATH_SCRIPT=%BABUN_HOME%\tools\setpath.vbs
+set LINK_SCRIPT=%BABUN_HOME%\tools\link.vbs
+
+:UNZIP
+ECHO [babun] Unzipping babun
+if not exist "%BABUN_HOME%" (mkdir "%BABUN_HOME%" || goto :ERROR)
+"%UNZIPPER%" "%BABUN_ZIP%" -d "%USERPROFILE%"
+if not exist "%BABUN_HOME%/*.*" (GOTO ERROR)
+
+:PATH
+ECHO [babun] Adding babun to the system PATH variable
+if not exist "%SETPATH_SCRIPT%" (
+    ECHO [babun] Cannot add babun to the system PATH variable. Script not found!
+    EXIT /b 255
+)
+cscript //Nologo "%SETPATH_SCRIPT%" "%BABUN_HOME%"
+
+:LINK
+ECHO [babun] Creating a desktop link
+if not exist "%LINK_SCRIPT%" (
+    ECHO [babun] Cannot create a desktop link. Script not found!
+    EXIT /b 255
+)
+cscript //Nologo "%LINK_SCRIPT%" "%USERPROFILE%\Desktop\babun.lnk" "%CYGWIN_HOME%\bin\mintty.exe" " - "
+
+:INSTALLED
+ECHO [babun] Babun installed successfully. You can delete the the installer.
+
+:RUN
+ECHO [babun] Starting babun
+start %CYGWIN_HOME%\bin\mintty.exe - || goto :ERROR
+GOTO END
+
+:ERROR
+ECHO [babun] Terminating due to internal error #%errorlevel%
+EXIT /b %errorlevel%
+
+:END
+

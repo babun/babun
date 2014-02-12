6e4790ce3b6f425219da7d3071cbac14e46bdbc9
Tom Bujok <tom.bujok@gmail.com>
2014-02-12 17:34:45 +0100

    find path fixed

--- babun-dist/symlinks/symlinks_find.sh
+++ babun-dist/symlinks/symlinks_find.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find /bin /dev /etc /lib /sbin /usr /var -type l > /etc/postinstall/symlinks_broken.txt
+/bin/find /bin /dev /etc /lib /sbin /usr /var -type l > /etc/postinstall/symlinks_broken.txt

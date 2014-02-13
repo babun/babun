306e559e9d7dfc3b35abcc5037e221450378fa6a
Tom Bujok <tom.bujok@gmail.com>
2014-02-13 13:29:39 +0100

    Auto invocation of fix symlinks on installation

--- babun-cygwin/symlinks/symlinks_repair.sh
+++ babun-cygwin/symlinks/symlinks_repair.sh
@@ -2,9 +2,8 @@
 filename="/etc/postinstall/symlinks_broken.txt"
 
 # verify if the file exist
-echo "Fixing broken symlinks"
 if [ ! -f $filename ]; then
-	echo "Cannot find file with broken links: $filename"
+	echo "Cannot find file with broken symlinks: $filename"
 	exit -1
 fi
 
@@ -14,16 +13,15 @@ fi
 i=0
 while read -r line
 do
-    echo "<- $line"
-    path=$(cygpath --windows "$line")
-    slashpath=${path//\\//}
-    array[ $i ]=$slashpath
+    array[ $i ]=$line
     (( i++ ))    
 done < "$filename"
 
 # set DOS SYSTEM flag on the symlink files
 for path in "${array[@]}"
 do
-  echo "-> $path"
-  cmd /c "attrib" "+s" "$path"
+  echo "Fixing symlink -> $path"
+  winpath=$(/bin/cygpath --windows "$path")
+  normpath=${winpath//\\//}
+  cmd /c "attrib" "+s" "$normpath"
 done

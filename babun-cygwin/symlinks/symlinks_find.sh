b0701143db5a22da5027d546f0ef6339f390b994
Tom Bujok <tom.bujok@gmail.com>
2014-02-13 11:15:45 +0100

    Removing xtype; Fixing new line char

--- babun-cygwin/symlinks/symlinks_find.sh
+++ babun-cygwin/symlinks/symlinks_find.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/bin/find /bin /dev /etc /lib /sbin /usr /var -type l -xtype l > /etc/postinstall/symlinks_broken.txt
+/bin/find /bin /dev /etc /lib /sbin /usr /var -type l > /etc/postinstall/symlinks_broken.txt

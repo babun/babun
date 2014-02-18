ff233f5d007f1e4c1c2538565339a73869b556c6
Tom Bujok <tom.bujok@gmail.com>
2014-02-18 10:32:54 +0100

    Core installer

--- babun-core/pact/install.sh
+++ babun-core/pact/install.sh
@@ -3,16 +3,17 @@ set -o pipefail
 set -e
 set -f
 
-PACT_SRC="~/.babun/babun-core/src"
-PACT_HOME="~/.pact"
+babun="/usr/local/etc/babun"
+src="$babun/babun-core/src"
+dest="~/.pact"
 
-\cp -rf $PACT_SRC/pact /usr/local/bin
+\cp -rf $src/pact /usr/local/bin
 chmod 755 /usr/local/bin/pact
 
-if [ ! -d "$PACT_HOME" ]; then
-    mkdir "$PACT_HOME"
+if [ ! -d "$dest" ]; then
+    mkdir "$dest"
 fi
 
-if [ ! -f "$PACT_HOME/pact.repo" ]; then
-    cp "$PACT_SRC/pact.repo" "$PACT_HOME"
+if [ ! -f "$dest/pact.repo" ]; then
+    cp "$src/pact.repo" "$dest"
 fi

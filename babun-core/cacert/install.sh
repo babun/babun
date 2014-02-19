2d8dca6f199f85a87ed407a7b6e137f066476d29
Tom Bujok <tom.bujok@gmail.com>
2014-02-19 16:13:37 +0100

    Adding cacert

---
+++ babun-core/cacert/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -o pipefail
+set -e
+set -f
+
+cd /usr/ssl/certs
+curl http://curl.haxx.se/ca/cacert.pem | awk 'split_after==1{n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1} {print > "cert" n ".pem"}'
+c_rehash

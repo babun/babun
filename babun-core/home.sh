d2d0104784004dd60524390b1cfc13334ba49261
Tom Bujok <tom.bujok@gmail.com>
2014-02-19 20:16:13 +0100

    Fixing zsh issues with compinit. Fixing zsh prompt

--- babun-core/home.sh
+++ babun-core/home.sh
@@ -10,6 +10,7 @@ eval homedir=$homedir
 
 /bin/cp -rf "$babun/home/." "$homedir"
 
+# add source .babunrc
 shells=("$homedir/.bashrc" "$homedir/.zshrc")
 for shell in "${shells[@]}"; do	
 	if ! grep -Fxq "source ~/.babunrc" "$shell" ;then
@@ -17,3 +18,17 @@ for shell in "${shells[@]}"; do
 		echo "source ~/.babunrc" >> "$shell"
 	fi
 done
+
+# add source .bashrc.prompt
+shells=("$homedir/.bashrc")
+for shell in "${shells[@]}"; do	
+	if ! grep -Fxq "source ~/.prompt" "$shell" ;then
+		echo "Supplementing prompt -> $shell"
+		echo "source ~/.prompt" >> "$shell"
+	fi
+done
+
+
+# fixing oh-my-zsh
+zsh -c "source ~/.zshrc; rm -f \"$homedir/.zcompdump\"; compinit"
+zsh -c "source ~/.zshrc; cat \"$homedir/.zcompdump\" > \"$homedir/.zcompdump-\"*"

c5e446629f65e4365929ab5b8e6271a00a112325
Tom Bujok <tom.bujok@gmail.com>
2014-02-23 20:48:06 +0100

    Fixing oh-my-zsh installation script

--- babun-core/oh-my-zsh/install_home.sh
+++ babun-core/oh-my-zsh/install_home.sh
@@ -8,15 +8,16 @@ source "$babun/source/babun-core/tools/plugins.sh"
 
 src="$babun/home/oh-my-zsh"
 
-# if oh-my-zsh not installed
-if [ ! -d "$homedir/.oh-my-zsh" ]; then	
-	
+if [ ! -d "$homedir/.oh-my-zsh" ]; then		
 	# installing oh-my-zsh
-    /bin/cp -rf "$src/.oh-my-zsh" "$homedir/.oh-my-zsh" 
-
-    # fixing oh-my-zsh components
-	zsh -c "source ~/.zshrc; rm -f \"$homedir/.zcompdump\"; compinit -u" &> /dev/null
-	zsh -c "source ~/.zshrc; cat \"$homedir/.zcompdump\" > \"$homedir/.zcompdump-\"*" &> /dev/null
-	
+    /bin/cp -rf "$src/.oh-my-zsh" "$homedir/.oh-my-zsh" 	
 fi
 
+
+if [ ! -f "$homedir/.zshrc" ]; then
+	/bin/cp "$babun/home/.zshrc" "$homedir/.zshrc" 
+
+	# fixing oh-my-zsh components
+	zsh -c "source ~/.zshrc; rm -f \"$homedir/.zcompdump\"; compinit -u" &> /dev/null
+	zsh -c "source ~/.zshrc; cat \"$homedir/.zcompdump\" > \"$homedir/.zcompdump-\"*" &> /dev/null
+fi

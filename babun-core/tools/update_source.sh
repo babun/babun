564426e67b9d21773badf6673a3839d11f356590
Tom Bujok <tom.bujok@gmail.com>
2014-02-24 19:31:29 +0100

    Update scripts' fixes

--- babun-core/tools/update_source.sh
+++ babun-core/tools/update_source.sh
@@ -6,8 +6,8 @@ set -f
 babun="/usr/local/etc/babun"
 
 echo "Fetching the newest version of babun"
-git -C "$babun/source" fetch --all
-git -C "$babun/source" reset --hard origin/master
+git --git-dir="$babun/source/.git" fetch --all
+git --git-dir="$babun/source/.git" reset --hard origin/master
 
 echo "Changing new line feeds"
 find "$babun/source/babun-core" -type f -exec dos2unix {} \;

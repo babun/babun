e7d7059eb51841c459f42d8ab75b34226fc265bf
Tom Bujok <tom.bujok@gmail.com>
2014-02-24 17:24:32 +0100

    Upgrade fixes

--- babun-core/tools/update_source.sh
+++ babun-core/tools/update_source.sh
@@ -8,3 +8,9 @@ babun="/usr/local/etc/babun"
 echo "Fetching the newest version of babun"
 git -C "$babun/source" fetch --all
 git -C "$babun/source" reset --hard origin/master
+
+echo "Changing new line feeds"
+find "$babun/source/babun-core" -type f -exec dos2unix {} \;
+
+echo "Making scripts executable"
+find "$babun/source/babun-core" -type f -regex '.*sh' -exec chmod 755 {} \;

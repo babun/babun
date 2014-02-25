7431e55ddfe71b702f12dcd8c6bd3364d0dafc7a
Tom Bujok <tom.bujok@gmail.com>
2014-02-25 13:30:05 +0100

    Update git fixes

--- babun-core/tools/update_source.sh
+++ babun-core/tools/update_source.sh
@@ -10,10 +10,16 @@ if [[ -z "$BABUN_BRANCH" ]]; then
 fi
 
 echo "Fetching the newest version of babun from [$BABUN_BRANCH]"
+for remote in `git --git-dir="$babun/source/.git" branch -r`; do
+    git branch --track $remote
+done
+
 git --git-dir="$babun/source/.git" fetch --all
+git --git-dir="$babun/source/.git" pull --all
 git --git-dir="$babun/source/.git" reset --hard
 git --git-dir="$babun/source/.git" checkout $BABUN_BRANCH
 git --git-dir="$babun/source/.git" clean -d -x -f -f
+
 
 echo "Changing new line feeds"
 find "$babun/source/babun-core" -type f -exec dos2unix -q {} \;

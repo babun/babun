f18440f920853a86d856578d03fbc01d1d541309
Tom Bujok <tom.bujok@gmail.com>
2014-02-25 13:09:40 +0100

    Making the update script quiet. Cleaning the git folder after branch switch

--- babun-core/tools/update_source.sh
+++ babun-core/tools/update_source.sh
@@ -9,13 +9,14 @@ if [[ -z "$BABUN_BRANCH" ]]; then
 	export BABUN_BRANCH=master
 fi
 
-echo "Fetching the newest version of babun"
+echo "Fetching the newest version of babun from [$BABUN_BRANCH]"
 git --git-dir="$babun/source/.git" fetch --all
-git --git-dir="$babun/source/.git" reset --hard origin/master
+git --git-dir="$babun/source/.git" reset --hard
 git --git-dir="$babun/source/.git" checkout $BABUN_BRANCH
+git --git-dir="$babun/source/.git" clean -d -x -f -f
 
 echo "Changing new line feeds"
-find "$babun/source/babun-core" -type f -exec dos2unix {} \;
+find "$babun/source/babun-core" -type f -exec dos2unix -q {} \;
 
 echo "Making scripts executable"
 find "$babun/source/babun-core" -type f -regex '.*sh' -exec chmod 755 {} \;

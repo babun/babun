9ec176ee48af9d8b3a6f817f0d1a9ded2a8ccb22
Tom Bujok <tom.bujok@gmail.com>
2014-02-27 18:03:21 +0100

    working version of procps

--- babun-core/tools/procps.sh
+++ babun-core/tools/procps.sh
@@ -1,10 +1,9 @@
 function check_only_one_running {
-
-	proc_name=$1
-	running_count=$( procps aux | grep "$proc_name" | grep -v "grep" | wc -l )
-	if ! [[ $running_count -gt 0 ]]; then
-		echo "You can run only one instance of [$proc_name]. Exitting!"
-		exit 1
-	fi
-
+    proc_name=$1
+    running_count=$( procps aux | grep "$proc_name" | grep -v "grep" | wc -l )
+    # procps aux will be shown as the process as well (that the reason why it's 2 not 1)
+    if [[ $running_count -gt 2 ]]; then
+            echo "You can only run one instance of $proc_name. Exitting!"
+            exit 1
+    fi
 }

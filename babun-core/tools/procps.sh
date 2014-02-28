db0883fc9e66bd83b5f777badc4a08d62a889495
ue62407 <tom.bujok@sbb.ch>
2014-02-28 10:41:42 +0100

    Changing update invocation

--- babun-core/tools/procps.sh
+++ babun-core/tools/procps.sh
@@ -3,7 +3,7 @@ function check_only_one_running {
     running_count=$( procps aux | grep "$proc_name" | grep -v "grep" | wc -l )
     # procps aux will be shown as the process as well (that the reason why it's 2 not 1)
     if [[ $running_count -gt 2 ]]; then
-            echo "You can only run one instance of $proc_name. Exitting!"
+            echo "You can only run one instance of $proc_name. Exiting!"
             exit 1
     fi
 }

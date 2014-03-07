e1884a35b40d799d1141752b4ef43bc28f60c860
ue62407 <tom.bujok@sbb.ch>
2014-03-07 10:32:29 +0100

    Adding note to the shell files

--- babun-core/core/src/babun.bash
+++ babun-core/core/src/babun.bash
@@ -1,2 +1,6 @@
+# IMPORTANT NOTE!
+# DO NOT MODIFY THIS FILE -> IT WILL BE OVERWRITTEN ON UPDATE
+# If you want to some options modify the following file: ~/.bashrc
+
 #PS1='\[\033[01;32m\]\u\[\033[01;34m\]::\[\033[01;31m\]\h \[\033[00;34m\]{ \[\033[01;34m\]\w \[\033[00;34m\]}\[\033[01;32m\]-> \[\033[00m\]'
 PS1="\[\033[01;32m\]\u\[\033[01;34m\]::\[\033[01;31m\]\h \[\033[00;34m\]{ \[\033[01;34m\]\w \[\033[00;34m\]}\[\033[01;32m\] \$( sed -E 's/(.*)\/(.*)/(\2)/' $( git rev-parse --git-dir )/HEAD 2> /dev/null ) -> \$) -> \[\033[00m\]"

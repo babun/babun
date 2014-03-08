4b3ff9c751077316ee07833abaec51174cbd8128
Tom Bujok <tom.bujok@gmail.com>
2014-03-08 20:21:30 +0100

    Other combinations of bash prompt - not ready

--- babun-core/core/src/babun.bash
+++ babun-core/core/src/babun.bash
@@ -8,5 +8,10 @@
 # slow git prompt
 #PS1="\[\033[01;32m\]\u\[\033[01;34m\]::\[\033[01;31m\]\h \[\033[00;34m\]{ \[\033[01;34m\]\w \[\033[00;34m\]}\[\033[01;32m\] \$( sed -E 's/(.*)\/(.*)/(\2)/' \$( git rev-parse --git-dir )/HEAD 2> /dev/null ) -> \[\033[00m\]"
 
+# other combinations
+# git branch 2> /dev/null | sed -n '/\* /s/.+/(\1)/p';
+# sed -E 's/(.*)\/(.*)/(\2)/' $( git rev-parse --git-dir 2> /dev/null)/HEAD 2> /dev/null
+# git rev-parse --abbrev-ref HEAD 2> /dev/null | sed -n "s/.+/(/1)/p"
+
 # quicker git prompt
 PS1="\[\033[01;32m\]\u\[\033[01;34m\]::\[\033[01;31m\]\h \[\033[00;34m\]{ \[\033[01;34m\]\w \[\033[00;34m\]}\[\033[01;32m\] \$( git branch 2> /dev/null ) -> \[\033[00m\]"

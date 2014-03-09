92f90166e7aed3fdc1b59cbc4602744ca5aa763f
Tom Bujok <tom.bujok@gmail.com>
2014-03-09 22:23:42 +0100

    Backup of mail notes

--- babun-core/core/src/babun.bash
+++ babun-core/core/src/babun.bash
@@ -13,5 +13,11 @@
 # sed -E 's/(.*)\/(.*)/(\2)/' $( git rev-parse --git-dir 2> /dev/null)/HEAD 2> /dev/null
 # git rev-parse --abbrev-ref HEAD 2> /dev/null | sed -n "s/.+/(/1)/p"
 
+# mkpasswd -l -c >> /etc/passwd; mkgroup -l -c >> /etc/group
+# https://github.com/mavnn/mintty-colors-solarized
+# https://github.com/seebi/dircolors-solarized
+# https://github.com/square/maximum-awesome/tree/master/iterm2-colors-solarized
+
+
 # quicker git prompt
 PS1="\[\033[01;32m\]\u\[\033[01;34m\]::\[\033[01;31m\]\h \[\033[00;34m\]{ \[\033[01;34m\]\w \[\033[00;34m\]}\[\033[01;32m\] \$( git branch 2> /dev/null ) -> \[\033[00m\]"

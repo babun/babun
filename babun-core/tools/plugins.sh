9bd54c32e67a93093ad4cb7d960ae89bcb0701f0
ue62407 <tom.bujok@sbb.ch>
2014-03-19 10:23:14 +0100

    Improving babun update output - plugin output - fix

--- babun-core/tools/plugins.sh
+++ babun-core/tools/plugins.sh
@@ -5,10 +5,12 @@ function should_install_plugin {
 
 	installed="/usr/local/etc/babun/installed/$plugin_name"
 	if [ -f "$installed" ]; then
-		echo "Plugin [$plugin_name]"
+		echo "$plugin_name"		
+		typeset -i installed_version=$(cat "$installed") || installed_version=0	
+
 		echo "  installed [$installed_version]"
 		echo "  newest    [$plugin_version]"
-		typeset -i installed_version=$(cat "$installed") || installed_version=0	
+
 		if ! [[ $plugin_version -gt $installed_version ]]; then
 			echo "  action [skip]"
 			exit 0

bb515da3efdf900746175dfef396c819662b744a
Tom Bujok <tom.bujok@gmail.com>
2014-03-25 23:44:53 +0100

    Fix #62 Script lock does not always work

--- babun-core/tools/procps.sh
+++ babun-core/tools/procps.sh
@@ -1,9 +1,13 @@
 function check_only_one_running {
+    if ! [ -d /var/lock ]; then
+        mkdir -p /var/lock
+    fi
+
     proc_name=$1
-    running_count=$( procps aux | grep "$proc_name" | grep -v "grep" | wc -l )
-    # procps aux will be shown as the process as well (that the reason why it's 2 not 1)
-    if [[ $running_count -gt 2 ]]; then
-            echo "You can only run one instance of $proc_name. Exiting!"
-            exit 1
+    if mkdir /var/lock/$proc_name 2>/dev/null ; then
+        trap 'rm -rf "/var/lock/$proc_name"; exit $?' INT TERM EXIT
+    else
+        echo "You can only run one instance of $proc_name. Exiting!";
+        exit 1;
     fi
 }

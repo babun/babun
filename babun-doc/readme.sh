53715d769e89507f421434aa828bce4b319da647
Tom Bujok <tom.bujok@gmail.com>
2014-04-05 15:34:20 +0200

    Readme structure adjusted

--- babun-doc/readme.sh
+++ babun-doc/readme.sh
@@ -2,6 +2,7 @@ readme="../README.adoc"
 
 echo "" > "$readme"
 
-cat adoc/preface.adoc >> "$readme"
+cat adoc/header.adoc >> "$readme"
 cat adoc/usage.adoc >> "$readme"
 cat adoc/development.adoc >> "$readme"
+cat adoc/footer.adoc >> "$readme"

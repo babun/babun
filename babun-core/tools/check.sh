925847576c81bd99fc40b32bca1ac1a72858616d
Tom Bujok <tom.bujok@gmail.com>
2014-05-10 00:46:33 +0200

    Babun version check link changed

--- babun-core/tools/check.sh
+++ babun-core/tools/check.sh
@@ -16,7 +16,7 @@ function get_newest_version {
 	if [[ -z $CHECK_TIMEOUT_IN_SECS ]]; then 
 		CHECK_TIMEOUT_IN_SECS=4
 	fi
-	local newest_version=$( curl --silent --connect-timeout $CHECK_TIMEOUT_IN_SECS https://raw.github.com/babun/babun/$BABUN_BRANCH/babun.version || echo "" )
+	local newest_version=$( curl --silent --connect-timeout $CHECK_TIMEOUT_IN_SECS https://raw.githubusercontent.com/babun/babun/$BABUN_BRANCH/babun.version || echo "" )
 	echo "$newest_version"
 }
 

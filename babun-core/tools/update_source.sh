4636455f84228d9e1ade02e02750cd089556a8e1
Tom Bujok <tom.bujok@gmail.com>
2014-05-11 18:35:42 +0200

    Adding --force option to babun update

--- babun-core/tools/update_source.sh
+++ babun-core/tools/update_source.sh
@@ -4,29 +4,32 @@ source "/usr/local/etc/babun.instance"
 source "$babun_tools/script.sh"
 source "$babun_tools/check.sh"
 
+local option="$1"
+
 if [[ -z "$BABUN_BRANCH" ]]; then
 	export BABUN_BRANCH=release
 fi
-
 echo "  upstream  [$BABUN_BRANCH]"
 
-installed_version_string=$( get_current_version )
-newest_version_string=$( get_newest_version )
+if [[ "$option" != "--force" ]]; then
+	installed_version_string=$( get_current_version )
+	newest_version_string=$( get_newest_version )
 
-if [[ -z "$newest_version_string" ]]; then 
-	echo "ERROR: Cannot fetch the newest version from github. Are you behind a proxy? Execute 'babun check' to find out."
-	exit -1
-fi
+	if [[ -z "$newest_version_string" ]]; then 
+		echo "ERROR: Cannot fetch the newest version from github. Are you behind a proxy? Execute 'babun check' to find out."
+		exit -1
+	fi
 
-installed_version=$( get_version_as_number "$installed_version_string" )
-newest_version=$( get_version_as_number "$newest_version_string" )
+	installed_version=$( get_version_as_number "$installed_version_string" )
+	newest_version=$( get_version_as_number "$newest_version_string" )
 
-echo "  installed [$installed_version_string]"
-echo "  newest    [$newest_version_string]"
+	echo "  installed [$installed_version_string]"
+	echo "  newest    [$newest_version_string]"
 
-if ! [[ $newest_version -gt $installed_version ]]; then
-	echo "Babun is up to date"
-	exit 0
+	if ! [[ $newest_version -gt $installed_version ]]; then
+		echo "Babun is up to date"
+		exit 0
+	fi
 fi
 
 git --git-dir="$babun/source/.git" --work-tree="$babun/source" reset --hard

08aca8a30cf13e135d1c05bf90d2d95b4b04abef
Tom Bujok <tom.bujok@gmail.com>
2014-05-24 01:39:35 +0200

    Fixing the problem of space in the user name - work in progress

--- babun-core/plugins/core/src/babun.zsh
+++ babun-core/plugins/core/src/babun.zsh
@@ -2,6 +2,7 @@
 # IMPORTANT NOTE!
 # DO NOT MODIFY THIS FILE -> IT WILL BE OVERWRITTEN ON UPDATE
 # If you want to some options modify the following file: ~/.zshrc
+source "/usr/local/etc/babun.instance"
 
 # overwrite values with user's local settings
-test -f ~/.babunrc && source ~/.babunrc
+test -f "$homedir/.babunrc" && source "$homedir/.babunrc"
--- babun-core/tools/init.sh
+++ babun-core/tools/init.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 set -e -f -o pipefail
 
-babun_instance="/usr/local/etc/babun.instance"
-
-cat /dev/null > "$babun_instance"
-echo 'babun=/usr/local/etc/babun' >> "$babun_instance"
-echo 'babun_source="$babun/source"' >> "$babun_instance"
-echo 'babun_plugins="$babun/source/babun-core/plugins"' >> "$babun_instance"
-echo 'babun_tools="$babun/source/babun-core/tools"' >> "$babun_instance"
-
-echo 'homedir=~' >> "$babun_instance"
-echo 'eval homedir="$homedir"' >> "$babun_instance"
-
-
+# No content for now

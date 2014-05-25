e8cf5cb5cffce095244688b370574a40df47b0fe
Tom Bujok <tom.bujok@gmail.com>
2014-05-25 19:18:13 +0200

    Fixing space problems #74

--- babun-core/tools/post_extract.sh
+++ babun-core/tools/post_extract.sh
@@ -11,6 +11,7 @@ source "$babun_tools/script.sh"
 # regenerate user/group information
 /bin/rm -rf /home
 /bin/mkpasswd.exe -l -c > /etc/passwd
+
 /bin/mkgroup -l -c > /etc/group
 
 # remove spaces in username and user home folder (sic!)
@@ -19,4 +20,5 @@ xhome="\/home\/"
 /bin/sed -e "s/$USERNAME/$xuser/" -e "s/$xhome$USERNAME/$xhome$xuser/" -i /etc/passwd
 
 # fix file permissions
+/bin/chmod 755 -R /usr/local
 /bin/sed -e "s/binary,/binary,noacl,/" -i /etc/fstab

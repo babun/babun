d43eba9412de08b43d2f676768eae0004922b077
Tom Bujok <tom.bujok@gmail.com>
2014-05-27 23:31:25 +0200

    Solving the problem of 'mintty -' parameter once and for all - login shell by default

--- babun-core/plugins/core/src/babun.zsh
+++ babun-core/plugins/core/src/babun.zsh
@@ -2,6 +2,7 @@
 # IMPORTANT NOTE!
 # DO NOT MODIFY THIS FILE -> IT WILL BE OVERWRITTEN ON UPDATE
 # If you want to some options modify the following file: ~/.zshrc
+source /etc/zprofile
 source "/usr/local/etc/babun.instance"
 
 # overwrite values with user's local settings

abece1327cac0896ea664f00a43d423f79c89777
Tom Bujok <tom.bujok@gmail.com>
2014-05-29 07:57:22 +0200

    Adding -global to git.sh

--- babun-core/tools/git.sh
+++ babun-core/tools/git.sh
@@ -1,5 +1,5 @@
-push=$(git config push.default || echo "")
+push=$(git config --global push.default || echo "")
 if [[ "$push" == "simple" ]]; then
     echo "WARNING: Git push strategy set to 'simple' which is unsupported - changing to 'matching'"
-    git config push.default "matching" || echo "ERROR: Cannot set git push.default to 'matching' - may cause problems..."
+    git config --global push.default "matching" || echo "ERROR: Cannot set git push.default to 'matching' - may cause problems..."
 fi

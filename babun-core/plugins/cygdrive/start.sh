c1211f57f71eeeb4f561e0a7178e85b0b0ffa33a
Lukasz Pielak <pielak@gmail.com>
2014-08-13 10:23:39 +0200

    bugfix, for loop does not find any files

--- babun-core/plugins/cygdrive/start.sh
+++ babun-core/plugins/cygdrive/start.sh
@@ -3,18 +3,20 @@ set -e -f -o pipefail
 source "/usr/local/etc/babun.instance"
 source "$babun_tools/script.sh"
 
-for root_dir in /*
-do
+for root_dir in $(find / -maxdepth 1 -type l)
+do		
 	link_target=$(readlink $root_dir)
 
 	if [[ "$link_target" =~ ^\/cygdrive\/.$ ]]; then
-		rm "$root_dir"
+		rm "$root_dir"	
 	fi
 done
 
-for cygdrive_dir in /cygdrive/*
+for cygdrive_dir in $(find /cygdrive/ -maxdepth 1 -type d)
 do
 	drive_name=$(basename $cygdrive_dir)
 
-	ln -s "$cygdrive_dir" "/$drive_name"
+	if [[ "$drive_name" != "cygdrive" ]]; then
+		ln -s "$cygdrive_dir" "/$drive_name"
+	fi
 done

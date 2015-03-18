7e37ae689562e1c8d42320d4f8720835ca4a11c5
Tom Bujok <tom.bujok@gmail.com>
2015-03-18 18:58:15 +0100

    Fixed build problems; Fixed #247

--- babun-core/plugins/git/start.sh
+++ babun-core/plugins/git/start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 source "/usr/local/etc/babun.instance"
-
-push=$(git config --global push.default || echo "")
-if [[ "$push" != "" && "$push" != "tracking" && "$push" != "matching" && "$push" != "current" ]]; then
-    echo "WARNING: Git push strategy set to 'simple' which is unsupported - changing to 'matching'"
-    git config --global push.default "matching" || echo "ERROR: Cannot set git push.default to 'matching' - may cause problems..."
-fi
+source "$babun_tools/script.sh"
+source "$babun_tools/git.sh"
---
+++ babun-core/tools/git.sh
@@ -0,0 +1,14 @@
+local push=$(git config --global push.default || echo "")
+local version=$(git --version || echo "")
+
+if [[ $version == *" 1."* ]]; then
+	if [[ "$push" != "nothing" && "$push" != "matching" && "$push" != "tracking" && "$push" != "current" ]]; then
+		echo "WARNING: Git push strategy set to [$push] which is unsupported - changing to 'matching'"
+	    git config --global push.default "matching" || echo "ERROR: Cannot set git push.default to 'matching' - may cause problems..."
+	fi
+else 
+	if [[ "$push" != "nothing" && "$push" != "matching" && "$push" != "simple" && "$push" != "upstream" && "$push" != "current" ]]; then
+		echo "WARNING: Git push strategy set to $push which is unsupported - changing to 'matching'"
+	    git config --global push.default "matching" || echo "ERROR: Cannot set git push.default to 'matching' - may cause problems..."
+	fi
+fi

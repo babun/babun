a9ea2553a31b0355d9a7ef7bc2bafc7fe9879559
Tom Bujok <tom.bujok@gmail.com>
2015-03-19 16:50:36 +0100

    Fixing oh-my-zsh installation - build problems ok

--- babun-core/plugins/oh-my-zsh/install.sh
+++ babun-core/plugins/oh-my-zsh/install.sh
@@ -9,10 +9,10 @@ dest="$babun/home/oh-my-zsh/.oh-my-zsh"
 
 if [ ! -d "$src" ]; then
 	PATH=/usr/bin git clone https://github.com/robbyrussell/oh-my-zsh.git "$src" 
-	git --git-dir="$dest/.git" --work-tree="$dest" config core.trustctime false 
-	git --git-dir="$dest/.git" --work-tree="$dest" config core.autocrlf false 
-	git --git-dir="$dest/.git" --work-tree="$dest" rm --cached -r . > /dev/null
-	git --git-dir="$dest/.git" --work-tree="$dest" reset --hard 
+	git --git-dir="$src/.git" --work-tree="$src" config core.trustctime false 
+	git --git-dir="$src/.git" --work-tree="$src" config core.autocrlf false 
+	git --git-dir="$src/.git" --work-tree="$src" rm --cached -r . > /dev/null
+	git --git-dir="$src/.git" --work-tree="$src" reset --hard 
 fi
 
 if [ ! -d "$dest" ]; then

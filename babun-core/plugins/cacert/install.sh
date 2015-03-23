1a26951a6ab38dbbf3c196d8e42c44ceae37447f
Tom Bujok <tom.bujok@gmail.com>
2015-03-23 11:18:56 +0100

    Possible problem with c_rehash

--- babun-core/plugins/cacert/install.sh
+++ babun-core/plugins/cacert/install.sh
@@ -5,4 +5,7 @@ source "$babun_tools/script.sh"
 
 cd /usr/ssl/certs
 curl http://curl.haxx.se/ca/cacert.pem | awk 'split_after==1{n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1} {print > "cert" n ".pem"}'
-c_rehash
+#c_rehash
+
+#possible problem with c_rehash in newest cygwin
+#for file in *.pem; do ln -s $file `openssl x509 -hash -noout -in $file`.0; done

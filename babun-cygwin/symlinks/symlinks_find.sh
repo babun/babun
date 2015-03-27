e17f4f134ec0baad2ca23210518afffc54be131b
Tom Bujok <tom.bujok@gmail.com>
2015-03-27 17:03:28 +0100

    Build for 1.7.35

--- babun-cygwin/symlinks/symlinks_find.sh
+++ babun-cygwin/symlinks/symlinks_find.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #set -e -f -o pipefail -> don't use it, not required
-/bin/find /bin /dev /etc /lib /sbin /usr /var -type l > /etc/postinstall/symlinks_broken.txt
+/bin/find /bin /dev /etc /lib /usr /var -type l > /etc/postinstall/symlinks_broken.txt

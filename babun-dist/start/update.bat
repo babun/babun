5565374c9d150867e4d569405ac53b650cd4cf68
Lukasz Pielak <pielak@gmail.com>
2015-03-28 07:50:56 +0100

    fixing update script in case of babun's home folder contains spaces

--- babun-dist/start/update.bat
+++ babun-dist/start/update.bat
@@ -64,18 +64,19 @@ if "%MIRROR%"=="" (
 echo [babun] Upgrading Cygwin from %MIRROR%
 echo [babun] Writing data to %DIST_DIR%
 
-%BASH% -c "source ~/.babunrc; /bin/rm.exe -f '%DIST_DIR%/setup-x86.exe' '%DIST_DIR%/cygwin.version'" || goto :ERROR
-%BASH% -c "source ~/.babunrc; /bin/wget.exe --directory-prefix='%DIST_DIR%' https://raw.githubusercontent.com/babun/babun-cygwin/master/cygwin.version" || goto :ERROR
+"%BASH%" -c "source ~/.babunrc; /bin/rm.exe -f '%DIST_DIR%/setup-x86.exe' '%DIST_DIR%/cygwin.version'" || goto :ERROR
+echo download cyg version
+"%BASH%" -c "source ~/.babunrc; /bin/wget.exe --directory-prefix='%DIST_DIR%' https://raw.githubusercontent.com/babun/babun-cygwin/master/cygwin.version" || goto :ERROR
 set /p CYGWIN_VERSION=<"%DIST_DIR%/cygwin.version"
 echo [babun] Downloading Cygwin %CYGWIN_VERSION%
-%BASH% -c "source ~/.babunrc; /bin/wget.exe --directory-prefix='%DIST_DIR%' https://raw.githubusercontent.com/babun/babun-cygwin/%CYGWIN_VERSION%/babun-cygwin/setup-x86.exe" || goto :ERROR
+"%BASH%" -c "source ~/.babunrc; /bin/wget.exe --directory-prefix='%DIST_DIR%' https://raw.githubusercontent.com/babun/babun-cygwin/%CYGWIN_VERSION%/babun-cygwin/setup-x86.exe" || goto :ERROR
 
 :SETUPRC
 echo [babun] Preparing setup.rc config
-%BASH% -c "source ~/.babunrc; /bin/rm.exe -f /etc/setup/setup.rc" || goto :ERROR
+"%BASH%" -c "source ~/.babunrc; /bin/rm.exe -f /etc/setup/setup.rc" || goto :ERROR
 
 :RUNNINGCHECK
-%BASH% -c "source ~/.babunrc; /bin/ps.exe | /bin/grep.exe /usr/bin/mintty | /bin/wc.exe -l" > "%DIST_DIR%/running_count"
+"%BASH%" -c "source ~/.babunrc; /bin/ps.exe | /bin/grep.exe /usr/bin/mintty | /bin/wc.exe -l" > "%DIST_DIR%/running_count"
 set /p RUNNING_COUNT=<"%DIST_DIR%/running_count"	
 
 if NOT "%RUNNING_COUNT%"=="0" (
@@ -83,17 +84,17 @@ if NOT "%RUNNING_COUNT%"=="0" (
 	GOTO BABUNRUNNING
 )
 
-%BASH% -c "source ~/.babunrc; /bin/rm.exe -f /etc/setup/setup.rc;" || goto :ERROR
+"%BASH%" -c "source ~/.babunrc; /bin/rm.exe -f /etc/setup/setup.rc;" || goto :ERROR
 rem Due to bug in setup.exe's quiet-mode the /etc/setup/setup.rc file is not read anyway...
-rem %BASH% -c "source ~/.babunrc; /bin/echo.exe 'net-method' >> /etc/setup/setup.rc" || goto :ERROR
-rem %BASH% -c "source ~/.babunrc; /bin/echo.exe '	IE' >> /etc/setup/setup.rc" || goto :ERROR
-rem %BASH% -c "source ~/.babunrc; /bin/echo.exe 'last-action' >> /etc/setup/setup.rc" || goto :ERROR
-rem %BASH% -c "source ~/.babunrc; /bin/echo.exe '	Download,Install' >> /etc/setup/setup.rc" || goto :ERROR
+rem "%BASH%" -c "source ~/.babunrc; /bin/echo.exe 'net-method' >> /etc/setup/setup.rc" || goto :ERROR
+rem "%BASH%" -c "source ~/.babunrc; /bin/echo.exe '	IE' >> /etc/setup/setup.rc" || goto :ERROR
+rem "%BASH%" -c "source ~/.babunrc; /bin/echo.exe 'last-action' >> /etc/setup/setup.rc" || goto :ERROR
+rem "%BASH%" -c "source ~/.babunrc; /bin/echo.exe '	Download,Install' >> /etc/setup/setup.rc" || goto :ERROR
 
 :SETUPPROXY
 if "%PROXY%"=="" (
 	echo [babun] Proxy flag not set, trying to read the proxy from ~/.babunrc
-	%BASH% -c "/bin/grep.exe 'export http_proxy=' ~/.babunrc | /bin/grep.exe -v '#' | /bin/sed.exe 's/export http_proxy=//g' | /bin/cut.exe -d "@" -f 2 " > "%DIST_DIR%/proxy" 
+	"%BASH%" -c "/bin/grep.exe 'export http_proxy=' ~/.babunrc | /bin/grep.exe -v '#' | /bin/sed.exe 's/export http_proxy=//g' | /bin/cut.exe -d "@" -f 2 " > "%DIST_DIR%/proxy" 
 	set /p PROXY=<"%DIST_DIR%/proxy"	
 )
 
@@ -112,7 +113,7 @@ GOTO VERSION
 :PROXYDOWNLOAD
 cd "%DIST_DIR%"
 echo [babun] Executing Cygwin upgrade with proxy=%PROXY%
-setup-x86.exe --quiet-mode --upgrade-also --site="%MIRROR%" --no-admin --no-shortcuts --no-startmenu --no-desktop --root="%CYGWIN_HOME%" --local-package-dir="%DIST_DIR%" --proxy=%PROXY% || goto :ERROR
+setup-x86.exe --quiet-mode --upgrade-also --site="%MIRROR%" --no-admin --no-shortcuts --no-startmenu --no-desktop --root="%CYGWIN_HOME%" --local-package-dir="%DIST_DIR%" --proxy="%PROXY%" || goto :ERROR
 GOTO VERSION
 
 :VERSION

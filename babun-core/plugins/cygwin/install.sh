fe290c027ad0a025b622203af907e6136339f932
Tom Bujok <tom.bujok@gmail.com>
2015-03-30 12:34:06 +0200

    Fixing fu*** mkgroup.exe newest cygwin problems

--- babun-core/plugins/cygwin/install.sh
+++ babun-core/plugins/cygwin/install.sh
@@ -4,9 +4,13 @@ source "/usr/local/etc/babun.instance"
 source "$babun_tools/script.sh"
 
 src="$babun_source/babun-core/plugins/cygwin/src"
-dest="$babun/home/pact/.pact"
-
 
 /bin/cp -rf /bin/mkpasswd.exe /bin/mkpasswd.exe.current
 /bin/cp -rf $src/bin/mkpasswd_1.7.29.exe /bin/mkpasswd.exe
-chmod 755 /bin/mkpasswd.exe /bin
+chmod 755 /bin/mkpasswd.exe 
+
+/bin/cp -rf /bin/mkgroup.exe /bin/mkgroup.exe.current
+/bin/cp -rf $src/bin/mkgroup_1.7.29.exe /bin/mkgroup.exe
+chmod 755 /bin/mkgroup.exe
+
+

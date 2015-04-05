1fd5d7d0075d5eab76641d85a5a70b1320f5f519
kubamarchwicki <kuba@marchwicki.pl>
2015-04-05 22:23:01 +0200

    Removing multiple sourcing of /etc/zprofile as adviced in #205

--- babun-core/plugins/core/src/babun.zsh
+++ babun-core/plugins/core/src/babun.zsh
@@ -2,7 +2,6 @@
 # IMPORTANT NOTE!
 # DO NOT MODIFY THIS FILE -> IT WILL BE OVERWRITTEN ON UPDATE
 # If you want to some options modify the following file: ~/.zshrc
-source /etc/zprofile
 test -f "$homedir/.zprofile" && source "$homedir/.zprofile"
 source "/usr/local/etc/babun.instance"
 

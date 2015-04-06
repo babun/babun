7bc7a2ae9e522e574909356aba8cf4926839dae9
Kuba Marchwicki <kuba.marchwicki@gmail.com>
2015-04-06 18:16:37 +0200

    Babun Shell here plugin - registry modification

---
+++ babun-core/plugins/shell-here/install.sh
@@ -0,0 +1,17 @@
+set -e -f -o pipefail
+source "/usr/local/etc/babun.instance"
+source "$babun_tools/script.sh"
+
+name="Babun Shell here"
+cmd="%HOMEDRIVE%%HOMEPATH%\.babun\cygwin\bin\mintty.exe -e /bin/xhere /bin/zsh.exe"
+
+keys=("HKCU\Software\Classes\Directory\Background\shell\babun"
+	"HKCU\Software\Classes\Directory\shell\babun"
+	"HKCU\Software\Classes\Drive\Background\Shell\babun"
+	"HKCU\Software\Classes\Drive\shell\babun")
+
+for key in ${keys[*]}
+do 
+	cmd /c "reg" "add" "${key}" "/ve" "/d" "${name}" "/t" "REG_SZ" "/f"
+	cmd /c "reg" "add" "${key}\command" "/ve" "/d" "${cmd}" "/t" "REG_EXPAND_SZ" "/f"
+done

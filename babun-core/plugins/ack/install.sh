71ce82a98d371317be369edc8a9a0f781b549993
Lukasz Pielak <pielak@gmail.com>
2015-04-21 18:17:12 +0200

    wrapping ack source file into a tarball

--- babun-core/plugins/ack/install.sh
+++ babun-core/plugins/ack/install.sh
@@ -3,5 +3,6 @@ set -e -f -o pipefail
 source "/usr/local/etc/babun.instance"
 source "$babun_tools/script.sh"
 
+tar -C "$babun_plugins/ack/src/" -xf "$babun_plugins/ack/src/ack-214-single.tar" 
 /bin/cp -rf "$babun_plugins/ack/src/ack-214-single" /usr/local/bin/ack
 chmod 755 /usr/local/bin/ack

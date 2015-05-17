c2909de2555fede9dad010e72a24de93e9f48bf6
Lukasz Pielak <pielak@gmail.com>
2015-05-17 19:06:55 +0200

    bugfix shell-here not initialized on fresh install

--- babun-core/plugins/shell-here/install.sh
+++ babun-core/plugins/shell-here/install.sh
@@ -2,8 +2,4 @@ set -e -f -o pipefail
 source "/usr/local/etc/babun.instance"
 source "$babun_tools/script.sh"
 
-#start with installing chere
-pact install chere || echo "Installing 'chere' failed. Please execute 'pact install chere' to fix it otherwise the plugin may not work."
-
-#install registry keys
-"$babun_plugins/shell-here/exec.sh" init
+# NO LOGIC
--- babun-core/plugins/shell-here/install_home.sh
+++ babun-core/plugins/shell-here/install_home.sh
@@ -2,4 +2,8 @@ set -e -f -o pipefail
 source "/usr/local/etc/babun.instance"
 source "$babun_tools/script.sh"
 
-# NO LOGIC
+#start with installing chere
+pact install chere || echo "Installing 'chere' failed. Please execute 'pact install chere' to fix it otherwise the plugin may not work."
+
+#install registry keys
+"$babun_plugins/shell-here/exec.sh" init

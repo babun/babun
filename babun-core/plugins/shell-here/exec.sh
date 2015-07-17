fa93335749e68f2c6b5f65b404887648758ab49e
FichteFoll <fichtefoll2@googlemail.com>
2015-07-17 19:52:38 +0200

    shell-here: Add icon to menu items
    It's already there, so why not use it?

--- babun-core/plugins/shell-here/exec.sh
+++ babun-core/plugins/shell-here/exec.sh
@@ -5,6 +5,7 @@ function set_reg_keys {
 
 	local babun_root="$( cygpath -w "/" | sed "s#\\\cygwin##g" )"
 	# the name that appears in the right-click context menu
+	local icon="${babun_root}\cygwin\bin\mintty.exe"
 	local name="Open in Babun"
 	local background_name="Open Babun here"
 	local cmd="${babun_root}\babun.bat \"%1\""
@@ -17,13 +18,15 @@ function set_reg_keys {
 
 	#install registry keys
 	for key in ${keys[*]}
-	do 
+	do
 		cmd /c "reg" "add" "${key}" "/ve" "/d" "${name}" "/t" "REG_SZ" "/f" || echo "Failed adding ${key}"
+		cmd /c "reg" "add" "${key}" "/v" "Icon" "/d" "${icon}" "/t" "REG_SZ" "/f" || echo "Failed adding ${key}"
 		cmd /c "reg" "add" "${key}\command" "/ve" "/d" "${cmd}" "/t" "REG_EXPAND_SZ" "/f" || echo "Failed adding ${key}"
 	done
 	for key in ${background_keys[*]}
 	do
 		cmd /c "reg" "add" "${key}" "/ve" "/d" "${background_name}" "/t" "REG_SZ" "/f" || echo "Failed adding ${key}"
+		cmd /c "reg" "add" "${key}" "/v" "Icon" "/d" "${icon}" "/t" "REG_SZ" "/f" || echo "Failed adding ${key}"
 		cmd /c "reg" "add" "${key}\command" "/ve" "/d" "${background_cmd}" "/t" "REG_EXPAND_SZ" "/f" || echo "Failed adding ${key}"
 	done
 }
@@ -37,7 +40,7 @@ function unset_reg_keys {
 
 	#uninstall registry keys
 	for key in ${keys[*]}
-	do 
+	do
 		cmd /c "reg" "delete" "${key}" "/f" || echo "Failed deleting ${key}"
 	done
 
@@ -50,6 +53,6 @@ elif [ "$1" == "remove" ]; then
 	unset_reg_keys
 elif [ "$1" == "" ]; then
 	echo "Missing argument. Use 'init' or 'remove' option."
-else 
+else
 	echo "Invalid option $1. Valid options are 'init' and 'remove'."
 fi

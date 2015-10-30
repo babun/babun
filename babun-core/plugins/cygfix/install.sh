99281ac96245176564919f4a2a124ff651502d23
ue63662 <lukasz.pielak@sbb.ch>
2015-10-30 07:44:46 +0100

    workaround for #455. Replacing git-remote-http and git-remote-https with older version from git 2.1.4

--- babun-core/plugins/cygfix/install.sh
+++ babun-core/plugins/cygfix/install.sh
@@ -5,13 +5,26 @@ source "$babun_tools/script.sh"
 
 src="$babun_source/babun-core/plugins/cygfix/src"
 
+echo "Fixing mkpasswd.exe"
 /bin/cp -rf /bin/mkpasswd.exe /bin/mkpasswd.exe.current
 /bin/cp -rf $src/bin/mkpasswd_1.7.29.exe /bin/mkpasswd.exe
 chmod 755 /bin/mkpasswd.exe 
 
+echo "Fixing mkgroup.exe"
 /bin/cp -rf /bin/mkgroup.exe /bin/mkgroup.exe.current
 /bin/cp -rf $src/bin/mkgroup_1.7.29.exe /bin/mkgroup.exe
 chmod 755 /bin/mkgroup.exe
+
+# Fix for https://github.com/babun/babun/issues/455 (Git ntlm proxy issue)
+echo "Fixing git-remote-http.exe"
+/bin/cp -rf /usr/libexec/git-core/git-remote-http.exe /usr/libexec/git-core/git-remote-http.exe.current
+/bin/cp -rf $src/bin/git-remote-http_2.1.4.exe /usr/libexec/git-core/git-remote-http.exe
+chmod 755 /usr/libexec/git-core/git-remote-http.exe
+
+echo "Fixing git-remote-https.exe"
+/bin/cp -rf /usr/libexec/git-core/git-remote-http.exe /usr/libexec/git-core/git-remote-https.exe.current
+/bin/cp -rf $src/bin/git-remote-https_2.1.4.exe /usr/libexec/git-core/git-remote-https.exe
+chmod 755 /usr/libexec/git-core/git-remote-https.exe
 
 if [ ! -f "/bin/vi" ]
 then
